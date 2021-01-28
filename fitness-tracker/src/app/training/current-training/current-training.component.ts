f4f65115104a4071b6fbf7e3be3235784bdde6db
borisdedejski <borisdedejski@gmail.com>
2021-01-28 17:12:05 +0100

    (Lecture 52): Add modal dialog to prompt the user for training
    - On yes user can abandon the training

--- fitness-tracker/src/app/training/current-training/current-training.component.ts
+++ fitness-tracker/src/app/training/current-training/current-training.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+
+import {StopTrainingComponent} from "./stop-training.component";
 
 @Component({
-  selector: 'app-current-training',
-  templateUrl: './current-training.component.html',
-  styleUrls: ['./current-training.component.css']
+    selector: 'app-current-training',
+    templateUrl: './current-training.component.html',
+    styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+    progress = 0;
+    timer: number;
 
-  constructor() { }
+    constructor(private dialog: MatDialog) {
+        this.timer = 0;
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.timer = setInterval(() => {
+            this.progress = this.progress + 10;
+            if (this.progress >= 100) {
+                clearInterval(this.timer);
+            }
+        }, 1000);
+    }
 
+    onStop() {
+        clearInterval(this.timer);
+
+        const dialogRef = this.dialog.open(StopTrainingComponent, {
+            data: {
+                progress: this.progress
+            }
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            console.log(result);
+        });
+    }
 }
