65bc5a968d26ca1c352ae1a94c267f99a6e68f96
borisdedejski <borisdedejski@gmail.com>
2021-01-28 17:42:25 +0100

    (Lecture 60): Render different routes based on Authentication

--- fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,21 +1,31 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {AuthService} from "../../auth/Auth.service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
     selector: 'app-sidenav-list',
     templateUrl: './sidenav-list.component.html',
     styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
     @Output() closeSidenav = new EventEmitter();
+    isAuth = false;
+    authSubscription = new Subscription();
 
-    constructor() {
+    constructor(private authService: AuthService) {
     }
 
     ngOnInit(): void {
+        this.authSubscription = this.authService.authChange.subscribe(authStatus =>{
+            this.isAuth = authStatus;
+        });
+    }
+
+    ngOnDestroy() {
+        this.authSubscription.unsubscribe();
     }
 
     onClose() {
         this.closeSidenav.emit();
     }
-
 }
