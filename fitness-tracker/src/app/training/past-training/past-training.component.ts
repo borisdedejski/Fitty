26c303f1df81ae728a29143394e0f18bbedb5b14
borisdedejski <borisdedejski@gmail.com>
2021-01-29 13:36:00 +0100

    (Lecture 71): Adding the Angular Material Data Table
    - Adding the angular material data table

--- fitness-tracker/src/app/training/past-training/past-training.component.ts
+++ fitness-tracker/src/app/training/past-training/past-training.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from "@angular/material/table";
+import { Exercise } from "../exercise.module";
+import { TrainingService } from "../training.service";
+
 
 @Component({
   selector: 'app-past-training',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./past-training.component.css']
 })
 export class PastTrainingComponent implements OnInit {
+  //This names should match the names of table rows in the html file.
+  displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
+  //Automatically assumed Exercies list
+  dataSource = new MatTableDataSource<Exercise>();
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit(): void {
+    this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
   }
+
+
 
 }
