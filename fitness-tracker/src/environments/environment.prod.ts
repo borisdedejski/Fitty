943a749a626f7291edf0d37f226566f4ab51dc9d
borisdedejski <borisdedejski@gmail.com>
2021-01-29 17:54:09 +0100

    (Lecture 111): Build for production

--- fitness-tracker/src/environments/environment.prod.ts
+++ fitness-tracker/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBLTTkL4X6JaLuUh8ky1i22lj8R5prU8Go",
+    authDomain: "fitty-fitness-tracker.firebaseapp.com",
+    projectId: "fitty-fitness-tracker",
+    storageBucket: "fitty-fitness-tracker.appspot.com",
+    messagingSenderId: "746952922225",
+    appId: "1:746952922225:web:1a4ebe545603249d6637d0",
+    measurementId: "G-7JF8KWDEF5"
+  }
 };
