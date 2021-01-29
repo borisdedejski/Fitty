b297257c7ae96ef89c1d14dbf690831cd63d888d
borisdedejski <borisdedejski@gmail.com>
2021-01-29 14:02:43 +0100

    (Lecture 75): Adding a pagination to the Data table

--- fitness-tracker/src/app/training/past-training/past-training.component.ts
+++ fitness-tracker/src/app/training/past-training/past-training.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from "@angular/material/table";
 import { Exercise } from "../exercise.module";
 import { TrainingService } from "../training.service";
 import { MatSort } from "@angular/material/sort"
+import { MatPaginator } from "@angular/material/paginator";
 
 @Component({
   selector: 'app-past-training',
@@ -14,16 +15,22 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   //Automatically assumed Exercies list
   dataSource = new MatTableDataSource<Exercise>();
-
+  //Use Material UI table & Material UI Paginatior
   @ViewChild(MatSort) sort: MatSort = new MatSort();
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
   constructor(private trainingService: TrainingService) { }
 
   ngOnInit(): void {
     this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
   }
 
+  //Init Paginators & Tables
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+    if(this.paginator){
+      this.dataSource.paginator = this.paginator;
+    }
   }
 
   doFilter(filterValue: string) {
